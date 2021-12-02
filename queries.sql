1a37ae61e37028e0b843fcaa8a813b97fb780181
Busiwa24 <busiwaliuma@gmail.com>
2021-12-02 12:12:02 +0200

    Add queries for vets and animals

--- queries.sql
+++ queries.sql
@@ -12,6 +12,6 @@ SELECT name, escape_attempts FROM animals WHERE weight_kg>10.5;
 
 SELECT name FROM animals WHERE neutered IS TRUE;
 
-SELECT name FROM animals WHERE name NOT IN ('Gabumon');
+SELECT name FROM animals WHERE name NOT IN ('Gabumon');0
 
 SELECT name FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
